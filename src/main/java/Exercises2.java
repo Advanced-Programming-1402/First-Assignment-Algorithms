54dab74ed6a1adc28b014fd026fb2c41a079479e
ermia babaie <160699260+ermiababaie@users.noreply.github.com>
2024-03-01 00:32:01 +0330

    Update Exercises2.java

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -1,18 +1,7 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Exercises2 {
-
-    /*
-    Given an array of integers nums and an integer target, return indices of the two numbers
-    such that they add up to target.
-
-    You may assume that each input would have exactly one solution, and you may not use the same element twice.
-    You can return the answer in any order.
-    */
-
     public int[] twoSum(int[] nums, int target) {
         int ans[] = new int[2];
         for (int i = 0; i < nums.length; i++) {
@@ -57,6 +46,7 @@ public class Exercises2 {
     */
 
     public int romanToInt(String s) {
+        int n = s.length();
         int ans = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == 'I')
@@ -74,20 +64,71 @@ public class Exercises2 {
             else if (s.charAt(i) == 'M')
                 ans += 1000;
         }
+        for (int i = 0; i < (n - 1); i++) {
+            if (s.charAt(i) == 'I') {
+                if (s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'X')
+                    ans -= 2;
+                if (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C')
+                    ans -= 2;
+                if (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')
+                    ans -= 2;
+            }
+            else if (s.charAt(i) == 'V') {
+                if (s.charAt(i + 1) == 'X')
+                    ans -= 10;
+                if (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C')
+                    ans -= 10;
+                if (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')
+                    ans -= 10;
+            }
+            else if (s.charAt(i) == 'X') {
+                if (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C')
+                    ans -= 20;
+                if (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')
+                    ans -= 20;
+            }
+            else if (s.charAt(i) == 'L') {
+                if (s.charAt(i + 1) == 'C')
+                    ans -= 100;
+                if (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')
+                    ans -= 100;
+            }
+            else if (s.charAt(i) == 'C') {
+                if (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M')
+                    ans -= 200;
+            }
+            else if (s.charAt(i) == 'D') {
+                if (s.charAt(i + 1) == 'M')
+                    ans -= 1000;
+            }
+        }
         return ans;
     }
 
-    /*
-    Given an array nums of distinct integers, return all the possible permutations.
-    You can return the answer in any order.
-    */
-
     public List<List<Integer>> permute(int[] nums) {
-        // TODO
-        return null;
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> save = new ArrayList<Integer>();
+        save.add(nums[0]);
+        ans.add(save);
+        for (int i = 1; i < nums.length; i++) {
+            List<List<Integer>> save2 = new ArrayList<List<Integer>>();
+            for (int j = 0; j < ans.size(); j++) {
+                for (int k = 0; k <= i; k++) {
+                    List<Integer> save4 = new ArrayList<Integer>();
+                    for (int l = 0; l < k; l++)
+                        save4.add(ans.get(j).get(l));
+                    save4.add(nums[i]);
+                    for (int l = k; l <= (i - 1); l++)
+                        save4.add(ans.get(j).get(l));
+                    save2.add(save4);
+                }
+            }
+            ans = save2;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
-        // test your code here!
+
     }
 }

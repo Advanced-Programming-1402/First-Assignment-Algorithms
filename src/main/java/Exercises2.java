cf61c5cfb67f70be0ec36097356eaae9cc1d32c8
ermia babaie <160699260+ermiababaie@users.noreply.github.com>
2024-03-01 00:56:13 +0330

    Update Exercises2.java

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Exercises2 {
     public int[] twoSum(int[] nums, int target) {
-        int ans[] = new int[2];
+        int[] ans = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {

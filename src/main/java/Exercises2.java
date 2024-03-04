184e7e7ac049b2b233640c78a59786854a1e9fe2
ermia babaie <160699260+ermiababaie@users.noreply.github.com>
2024-03-04 12:20:55 +0330

    Update Exercises2.java

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -8,7 +8,7 @@ public class Exercises2 {
         int[] ans = new int[2];
         int cnt = 0;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
                     ans[0] = i;
                     ans[1] = j;
